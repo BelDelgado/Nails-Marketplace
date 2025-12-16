69535e60883248fe7a9f3d39e4a6ac255e6dcd67
BelDelgado <bel.delg94@gmail.com>
2025-12-16 12:52:18 -0300

    signal.py modificacion para el registro de usuario

--- nails-marketplace/project/apps/users/signals.py
+++ nails-marketplace/project/apps/users/signals.py
@@ -2,16 +2,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User, Profile, Reputation
 
-
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     """
     Crear automáticamente Profile y Reputation cuando se crea un usuario
     """
     if created:
-        Profile.objects.create(user=instance)
-        Reputation.objects.create(user=instance)
-
+        # get_or_create evita duplicados por si acaso
+        Profile.objects.get_or_create(user=instance)
+        Reputation.objects.get_or_create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
